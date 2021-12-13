b4c75c28e0fb1edabc82fdd389d062d5f609fd5d
Anton H <minnanikonnichiwa@gmail.com>
2021-12-13 23:41:44 +0200

    Added base project.

---
+++ src/Blazor.WA.API.Three-tierA.AAD.API/Controllers/APITestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Blazor.WA.API.Three_tierA.AAD.API.Controllers
+{
+    [Produces("application/json")]
+    [ApiController]
+    [Route("[controller]")]
+    public class APITestController : ControllerBase
+    {
+        public APITestController()
+        {
+        }
+
+        // GET: api/APITest
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[]
+            {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+        }
+    }
+}
--- src/Blazor.WA.API.Three-tierA.AAD.UI/Program.cs
+++ src/Blazor.WA.API.Three-tierA.AAD.UI/Program.cs
@@ -1,14 +1,10 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace Blazor.WA.API.Three_tierA.AAD
+namespace Blazor.WA.API.Three_tierA.AAD.UI
 {
     public class Program
     {
@@ -17,7 +13,7 @@ namespace Blazor.WA.API.Three_tierA.AAD
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44373/") });
 
             await builder.Build().RunAsync();
         }
